50d163b7d16236d74938b6524f177da71436da20
nara-ryoya <nara_ryoya@icloud.com>
2022-10-23 04:41:27 +0900

    fix: 環境変数先にダウンロード

--- backend/firebase_json.py
+++ backend/firebase_json.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 
 from settings import get_settings
@@ -10,7 +12,11 @@ def download_firebase_json_from_s3() -> None:
         aws_access_key_id=settings.aws_access_key_id,
         aws_secret_access_key=settings.aws_secret_access_key,
     )
-    client.download_file("goku-portfolio", "firebase_key.json", "firebase_key.json")
+    client.download_file(
+        "goku-portfolio",
+        "firebase_key.json",
+        os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
+    )
 
 
 if __name__ == "__main__":
