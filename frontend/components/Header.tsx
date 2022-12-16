42df019f0c314cdd0f22e4c11b5fc0f6d6dba342
nara-ryoya <nara_ryoya@icloud.com>
2022-12-17 02:29:40 +0900

    fix: レイアウトを修正

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,21 +1,26 @@
-import { Box, Divider } from "@chakra-ui/react";
-import { Flex } from "@chakra-ui/react";
+import { Box, Divider, HStack, VStack } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box bg="white.100" w="100%">
-      <Box p="4" pl="20" h="100px">
-        <Flex align="center" h="100%" color="blackAlpha.800">
-          <Box
-            color="blackAlpha.700"
-            bg="whiteAlpha.100"
-            fontWeight="900"
-            fontSize="4xl"
-          >
-            麻雀AI 極
-          </Box>
-        </Flex>
+    <Box bg="white.100" w="100%" pl="5"> 
+    <Flex flexDirection="row" align="center">
+      <Box h="100%" m="5">
+        <Image src="/goku-icon.png" boxSize="75px" />
       </Box>
+        <Box p="4" h="100px">
+          <Flex align="center" h="100%" color="blackAlpha.800">
+            <Box
+              color="blackAlpha.700"
+              bg="whiteAlpha.100"
+              fontWeight="900"
+              fontSize="4xl"
+            >
+              麻雀AI 極
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
       <Divider borderColor="gray.100" />
     </Box>
   );
