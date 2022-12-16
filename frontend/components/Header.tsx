45848a8d7918e011ed534493e84b3926596f52dc
nara-ryoya <nara_ryoya@icloud.com>
2022-12-17 02:44:48 +0900

    icon

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,11 +3,11 @@ import { Flex, Image } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box bg="white.100" w="100%" pl="5"> 
-    <Flex flexDirection="row" align="center">
-      <Box h="100%" m="5">
-        <Image src="/goku-icon.png" boxSize="75px" />
-      </Box>
+    <Box bg="white.100" w="100%" pl="5">
+      <Flex flexDirection="row" align="center">
+        <Box h="100%" m="5">
+          <Image src="/goku-icon.png" boxSize="75px" />
+        </Box>
         <Box p="4" h="100px">
           <Flex align="center" h="100%" color="blackAlpha.800">
             <Box
--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,16 +2,23 @@ import { Box, Flex, VStack } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { SimpleSidebar } from "./Sidebar";
 import Header from "./Header";
+import Head from "next/head";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <VStack w="100%" overflowX="scroll" overflowY="scroll">
-      <Header />
-      <Flex w="100%">
-        <SimpleSidebar />
-        <Box p="10">{children}</Box>
-      </Flex>
-    </VStack>
+    <>
+      <Head>
+        <title>麻雀AI 極</title>
+        <link rel="icon" href="/goku-icon.png" />
+      </Head>
+      <VStack w="100%" overflowX="scroll" overflowY="scroll">
+        <Header />
+        <Flex w="100%">
+          <SimpleSidebar />
+          <Box p="10">{children}</Box>
+        </Flex>
+      </VStack>
+    </>
   );
 };
 
